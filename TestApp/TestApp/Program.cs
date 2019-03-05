805efb068ffa425ea44e196f8d1842efc7b06fd5
ravetalexis <alexis.ravet@imie.fr>
2019-03-05 15:58:29 +0100

    ok ok

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int lot1 = 10;
+			int lot2 = 20;
+
+			int resut = testnum.Calcul(lot1, lot2);
+
+			Console.WriteLine(resut);
+
+			
+		}
+
+
+	}
+}
