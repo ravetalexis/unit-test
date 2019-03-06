250d3d1d17b86e6bcad470b1a2271b24c7eaf35f
ravetalexis <alexis.ravet@imie.fr>
2019-03-06 15:26:24 +0100

    install unitX

---
+++ TestApp/Test/UnitTest1.cs
@@ -0,0 +1,31 @@
+using System;
+using Xunit;
+
+
+namespace Test
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+			int lot1 = 10;
+			int lot2 = 20;
+
+			int result = TestApp.testnum.Calcul(lot1, lot2);
+
+			Equals(30, result);
+		}
+
+		[Fact]
+		public void Test2()
+		{
+			int lot1 = 10;
+			int lot2 = 20;
+
+			int result = TestApp.testnum.Calcul(lot1, lot2);
+
+			Equals(50, result);
+		}
+	}
+}
